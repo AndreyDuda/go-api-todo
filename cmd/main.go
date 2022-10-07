21d6724c4d176d47b88e4b614f11894886c55cbe
Andrey Duda <AndreyDuda88@gmail.com>
2022-10-08 00:41:14 +0300

    add connection to db

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/AndreyDuda/go-api-todo/pkg/handler"
 	"github.com/AndreyDuda/go-api-todo/pkg/repository"
 	"github.com/AndreyDuda/go-api-todo/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
 )
@@ -14,7 +15,20 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		UserName: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
