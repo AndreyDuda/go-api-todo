c7ab074f550591ec795fbc5709032755f7155e79
Andrey Duda <AndreyDuda88@gmail.com>
2022-10-14 01:13:08 +0300

    add crud for items

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -19,6 +19,11 @@ type Todolist interface {
 }
 
 type TodoItem interface {
+	Create(listId int, item todo.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]todo.TodoItem, error)
+	GetById(userId int, itemId int) (todo.TodoItem, error)
+	Delete(userId, itemId int) error
+	Update(userId int, itemId int, input todo.UpdateItemInput) error
 }
 
 type Repository struct {
@@ -31,5 +36,6 @@ func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{
 		Authorization: NewAuthPostgres(db),
 		Todolist:      NewTodoListPostgres(db),
+		TodoItem:      NewTodoItemPostgres(db),
 	}
 }
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -20,6 +20,11 @@ type Todolist interface {
 }
 
 type TodoItem interface {
+	Create(userId int, listId int, item todo.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]todo.TodoItem, error)
+	GetById(userId int, itemId int) (todo.TodoItem, error)
+	Delete(userId int, itemId int) error
+	Update(userId int, itemId int, input todo.UpdateItemInput) error
 }
 
 type Service struct {
@@ -32,5 +37,6 @@ func NewService(repos *repository.Repository) *Service {
 	return &Service{
 		Authorization: NewAutService(repos.Authorization),
 		Todolist:      NewTodoListService(repos.Todolist),
+		TodoItem:      NewTodoItemService(repos.TodoItem, repos.Todolist),
 	}
 }
